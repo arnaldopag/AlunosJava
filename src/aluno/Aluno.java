0925faf363157df00f09bb64ceb53bbb67389955
Arnaldo Pagani <arnaldopaganijr@gmail.com>
2021-07-22 12:34:13 -0300

    Não ta ruim mas podia ta melhor

--- src/aluno/Aluno.java
+++ src/aluno/Aluno.java
@@ -1,85 +1,61 @@
 package aluno;
 
+
+
 public class Aluno {
 	
-  
 	private String nome;
 	private int idade;
 	private int cod; 
-	private String disciplinas;
-	private double notas;
+	
+	private double media;
+	private String disciplina;
 	
 	
-	public Aluno(String nome, int idade, int cod, double notas, String disciplinas) {
-			super();
-			this.nome = nome;
-			this.idade = idade;
-			this.cod = cod;
-			this.disciplinas = disciplinas;
-			this.notas = notas;
-		
-		}
-
-
-
-	public String getNome() {
-		return nome;
+	public  Aluno(String nome, int idade, int cod, double media, String disciplina) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.cod = cod;
+		this.media = media;
+		this.disciplina = disciplina;
 	}
 
 
+
+	public  String getNome() {
+		return nome;
+	}
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
-
 	public int getIdade() {
 		return idade;
 	}
 
-
-
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-
-
-
 	public int getCod() {
 		return cod;
 	}
-
-
-
 	public void setCod(int cod) {
 		this.cod = cod;
 	}
-
-
-
-	public Double getNotas() {
-		return notas;
+	public double getMedia() {
+		return media;
 	}
-
-
-
-	public void setNotas(Double notas) {
-		this.notas = notas;
+	public void setMedia(double media) {
+		this.media = media;
 	}
-
-
-
-	public String getDisciplinas() {
-		return disciplinas;
+	public String getDisciplina() {
+		return disciplina;
 	}
-
-
-
-	public void setDisciplinas(String disciplinas) {
-		this.disciplinas = disciplinas;
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
 	}
-	
-	
-	
 }
+	
+	
--- src/aluno/Escola.java
+++ src/aluno/Escola.java
@@ -1,7 +1,6 @@
 package aluno;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.Scanner;
 
 
@@ -11,26 +10,44 @@ public class Escola {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		List<Aluno> list = new ArrayList<>();
-		ArrayList<String> disciplinas = new ArrayList<String>();
-		ArrayList<Double> notas = new ArrayList<Double>();
-		
-		char c;
-		do {
-		
-			Aluno al = new Aluno(nome, idade, cod,notas, disciplinas);
-			list.add(al);
+			List<Aluno> listaAluno = new ArrayList<>();
+			  
 			
-			System.out.print("Deseja cadastrar outro Aluno, S ou  N ");
-			 c = (char) sc.nextByte();
-		
-				}while(c == 's');
-		}
-
-
-
-	public static boolean hasCod(List<Aluno> list, int cod) {
-		Aluno emp = list.stream().filter(x -> x.getCod() == cod).findFirst().orElse(null);
-		return emp != null;
+			
+			System.out.println("Quantos alunos serão Cadastrados: ");
+			int n = sc.nextInt();
+			
+			for(int i = 0;i < n;i++) {
+				 System.out.print("Codigo Aluno: ");
+				    int cod = sc.nextInt();
+				    while (hasId(listaAluno, cod)) {
+						System.out.print("Código existente, digite outro: ");
+						cod = sc.nextInt();
+					}
+				System.out.print("Nome Aluno: ");
+				String nome = sc.next();
+			    System.out.print("Idade Aluno: ");
+			    int idade = sc.nextInt();
+			   
+			    System.out.print("Disciplina: ");
+			    String disciplina = sc.next();
+			    System.out.print("Media: ");
+			    double media = sc.nextDouble();
+			    
+			    
+			    
+			    Aluno al = new Aluno(nome, idade, cod,media,disciplina);
+			    listaAluno.add(al);
+		    	
+			    System.out.println("Nome: " + al.getNome());
+			    System.out.println("Código: " + al.getCod());
+			    System.out.println("Idade: " + al.getIdade());
+			    System.out.println("Nome: " + al.getDisciplina() + " Media: " + al.getMedia());
+			}
+			
+	   }
+	public static boolean hasId(List<Aluno> list, int cod) {
+		Aluno al = list.stream().filter(x -> x.getCod() == cod).findFirst().orElse(null);
+		return al != null;
 	}
 }
